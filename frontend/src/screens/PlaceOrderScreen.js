5744ab7aea4a6c7782500acfd3cb259e0871fb8e
Derry <ukderry@gmail.com>
2020-11-06 08:55:08 -0800

    updated shipping and tax price

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -21,10 +21,10 @@ const PlaceOrderScreen = ({ history }) => {
   cart.itemsPrice = addDecimals(
     cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
   )
-  cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100)
-  cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)))
+  cart.shippingPrice = addDecimals(cart.itemsPrice > 40 ? 0 : 10)
+  cart.taxPrice = addDecimals(Number((0.09 * cart.itemsPrice).toFixed(2)))
   cart.totalPrice = (
-    Number(cart.itemsPrice) +
+    Number(cart.itemsPrice) + 
     Number(cart.shippingPrice) +
     Number(cart.taxPrice)
   ).toFixed(2)
